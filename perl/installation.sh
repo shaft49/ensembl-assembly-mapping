56244858029198a1286f0ead9b82cc45fa906e0b
Shaafi <ishaafi2294@gmail.com>
2020-07-05 19:15:43 +0600

    change in installation script

--- perl/installation.sh
+++ perl/installation.sh
@@ -2,11 +2,10 @@
 cd ~
 mkdir src
 cd src
-apt update -y
-apt upgrade -y
-wget ftp://ftp.ensembl.org/pub/ensembl-api.tar.gz
-wget https://cpan.metacpan.org/authors/id/C/CJ/CJFIELDS/BioPerl-1.6.924.tar.gz
-wget https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/JSON-4.02.tar.gz
+sudo apt update -y
+sudo apt upgrade -y
+sudo wget ftp://ftp.ensembl.org/pub/ensembl-api.tar.gz
+sudo wget https://cpan.metacpan.org/authors/id/C/CJ/CJFIELDS/BioPerl-1.6.924.tar.gz
 tar zxvf ensembl-api.tar.gz
 tar zxvf BioPerl-1.6.924.tar.gz
 mv BioPerl-1.6.924/ bioperl-1.6.924
@@ -15,41 +14,41 @@ mv BioPerl-1.6.924/ bioperl-1.6.924
 cd ~
 mkdir perl-module-downloads
 cd perl-module-downloads
-wget https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/JSON-4.02.tar.gz
+sudo wget https://cpan.metacpan.org/authors/id/I/IS/ISHIGAKI/JSON-4.02.tar.gz
 tar zxvf JSON-4.02.tar.gz
 cd JSON-4.02
 perl Makefile.PL
-make
-make install
+sudo make
+sudo make install
 cd ..
 #JSON::XS
 cpan install JSON::XS
 cd ..
 
 # Devel::CheckLib
-wget https://cpan.metacpan.org/authors/id/M/MA/MATTN/Devel-CheckLib-1.14.tar.gz
+sudo wget https://cpan.metacpan.org/authors/id/M/MA/MATTN/Devel-CheckLib-1.14.tar.gz
 tar zxvf Devel-CheckLib-1.14.tar.gz
 cd Devel-CheckLib-1.14
 perl Makefile.PL
-make
-make install
+sudo make
+sudo make install
 cd ..
 
 #DBI
-wget https://cpan.metacpan.org/authors/id/T/TI/TIMB/DBI-1.643.tar.gz
+sudo wget https://cpan.metacpan.org/authors/id/T/TI/TIMB/DBI-1.643.tar.gz
 tar zxvf DBI-1.643.tar.gz
 cd DBI-1.643
 perl Makefile.PL
-make
-make install
+sudo make
+sudo make install
 cd ..
 
 #DBD
-wget https://cpan.metacpan.org/authors/id/D/DV/DVEEDEN/DBD-mysql-4.050.tar.gz
+sudo wget https://cpan.metacpan.org/authors/id/D/DV/DVEEDEN/DBD-mysql-4.050.tar.gz
 tar zxvf DBD-mysql-4.050.tar.gz
 cd DBD-mysql-4.050
 perl Makefile.PL
-make
-make install
+sudo make
+sudo make install
 cd ..
 
